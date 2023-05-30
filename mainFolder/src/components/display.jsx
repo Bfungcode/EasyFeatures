931cba4c884b6eb97cee18543d728c395814bd00
bfung <bfung0011@gmail.com>
2023-05-30 11:45:23 +0700

    Easy Features FINISH

--- mainFolder/src/components/display.jsx
+++ mainFolder/src/components/display.jsx
@@ -14,6 +14,7 @@ import {
   darcula,
   atomOneDark,
 } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import SearchIcon from "./page3";
 
 const Display = () => {
   const [copyJSX, setCopyJSX] = useState(false);
